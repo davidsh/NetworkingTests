5a96d2ef865e5a2295f2a94c9c771322203bb92d
David Shulman <david.shulman@microsoft.com>
2019-11-17 09:52:53 -0800

    Add Azure pipelines

--- testnetwork/EnterpriseTests/NegotiateStreamTest.cs
+++ testnetwork/EnterpriseTests/NegotiateStreamTest.cs
@@ -2,12 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
-using System.Linq;
-using System.Net.Test.Common;
-using System.Security.Authentication;
+using System.Net;
+using System.Net.Sockets;
 using System.Security.Principal;
-using System.Text;
 using System.Threading.Tasks;
 
 using Xunit;
@@ -35,36 +32,28 @@ namespace System.Net.Security.Tests
             //new object[] { CredentialCache.DefaultNetworkCredentials, true, "HOST/kdc.linux.contoso.com" },
         };
 
-        [Theory]
-        [MemberData(nameof(SuccessCasesMemberData))]
-        public async Task StreamToStream_Authentication_Success(
-            NetworkCredential creds, bool isKerberos, string target)
+        [Fact]
+        public async Task Client_ValidCreds_Success()
         {
-            var network = new VirtualNetwork();
+                var client = new TcpClient();
+                client.Connect(TestConfiguration.NegotiateServerHost, TestConfiguration.NegotiateServerPort);
+                // Ensure the client does not close when there is still data to be sent to the server.
+                client.LingerState = new LingerOption(true, 0);
 
-            using (var clientStream = new VirtualNetworkStream(network, isServer: false))
-            using (var serverStream = new VirtualNetworkStream(network, isServer: true))
-            using (var client = new NegotiateStream(clientStream))
-            using (var server = new NegotiateStream(serverStream))
-            {
-                Assert.False(client.IsAuthenticated);
-                Assert.False(server.IsAuthenticated);
+                // Request authentication.
+                NetworkStream clientStream = client.GetStream();
+                var authStream = new NegotiateStream(clientStream, false);
+                var credential = new NetworkCredential("user1", "password");
+                string target = "HTTP/linuxweb.linux.contoso.com";
+                await authStream.AuthenticateAsClientAsync(
+                    credential,
+                    target,
+                    ProtectionLevel.EncryptAndSign,
+                    TokenImpersonationLevel.Identification);
 
-                Task[] auth = new Task[2];
-                auth[0] = client.AuthenticateAsClientAsync(creds, target);
-                auth[1] = server.AuthenticateAsServerAsync();
-
-                await TestConfiguration.WhenAllOrAnyFailedWithTimeout(auth);
-
-                VerifyStreamProperties(client, isServer: false, isKerberos, target);
-
-                string remoteName = (creds == CredentialCache.DefaultNetworkCredentials) ?
-                    TestConfiguration.DefaultNetworkCredentials.UserName : creds.UserName;
-                if (isKerberos) remoteName += "@" + TestConfiguration.Realm;
-                //VerifyStreamProperties(server, isServer: true, isKerberos, remoteName);
-            }
+                VerifyStreamProperties(authStream, isServer: false, isKerberos: true, target);
         }
-        
+
         private void VerifyStreamProperties(NegotiateStream stream, bool isServer, bool isKerberos, string remoteName)
         {
             Assert.True(stream.IsAuthenticated);
@@ -93,25 +82,5 @@ namespace System.Net.Security.Tests
             // Invalid Kerberos credential password.
             new object[] { new NetworkCredential("user1", "passwordxx"), "HOST/localhost" },
         };
-
-        [Theory]
-        [MemberData(nameof(FailureCasesMemberData))]
-        public async Task StreamToStream_Authentication_Failure(NetworkCredential creds, string target)
-        {
-            var network = new VirtualNetwork();
-
-            using (var clientStream = new VirtualNetworkStream(network, isServer: false))
-            using (var serverStream = new VirtualNetworkStream(network, isServer: true))
-            using (var client = new NegotiateStream(clientStream))
-            using (var server = new NegotiateStream(serverStream))
-            {
-                Assert.False(client.IsAuthenticated);
-                Assert.False(server.IsAuthenticated);
-
-                AuthenticationException ex =
-                    await Assert.ThrowsAsync<AuthenticationException>(() => client.AuthenticateAsClientAsync(creds, target));
-                Console.WriteLine(ex.InnerException.Message);
-            }
-        }
     }
 }
--- testnetwork/EnterpriseTests/TestConfiguration.cs
+++ testnetwork/EnterpriseTests/TestConfiguration.cs
@@ -2,28 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Security.Authentication;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
-
-using Xunit;
-
 namespace System.Net.Security.Tests
 {
     internal static class TestConfiguration
     {
-        public const int PassingTestTimeoutMilliseconds = 4 * 60 * 1000;
-        public const int FailingTestTimeoutMiliseconds = 250;
-
         public const string Realm = "LINUX.CONTOSO.COM";
         public const string Domain = "LINUX";
+        public const string NegotiateServerHost = "linuxweb.linux.contoso.com";
+        public const int NegotiateServerPort = 8080;
 
         public static NetworkCredential DefaultNetworkCredentials { get { return new NetworkCredential("defaultcred", "password"); } }
-
-        public static Task WhenAllOrAnyFailedWithTimeout(params Task[] tasks)
-            => tasks.WhenAllOrAnyFailed(PassingTestTimeoutMilliseconds);
     }
 }
