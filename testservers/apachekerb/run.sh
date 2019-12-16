d58b87988b6398034e2806cdf010c2d214fe8e30
David Shulman <david.shulman@microsoft.com>
2019-12-15 19:23:30 -0800

    Add apache2 server

--- testservers/apachekerb/run.sh
+++ testservers/apachekerb/run.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
-
-#exec httpd -DFOREGROUND "$@"
-
 #!/usr/bin/env bash
 
 service krb5-kdc restart
 service krb5-admin-server restart
+service apache2 restart
 
 # Keep the container running
 tail -f /dev/null
